42a4b3d59d115d3616a362d1970595bf1c6789a4
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-06-01 22:43:18 -0300

    feat: Incluindo rota de delete

--- src/routes/libary.ts
+++ src/routes/libary.ts
@@ -18,13 +18,25 @@ export async function Libary(app: FastifyInstance) {
 
     app.post('/', (request, replay) => {
         try {
-            const bodyShema = z.object({title: z.string().min(2), author: z.string(), publishedYear: z.date(), read: z.boolean().default(false)})
+            const bodyShema = z.object({title: z.string().min(2), author: z.string(), publishedYear: z.date().default(new Date), read: z.boolean().default(false)})
             const body = bodyShema.parse(request.body);
 
             books.push({id: v4(), ...body});
             replay.status(201).send();
         } catch {
             replay.status(401).send("Impossível incluir este livro. Tente novamente mais tarde!")
+        }
+    })
+
+    app.delete('/:id', (request, replay) => {
+        try {
+            const idShema = z.object({id: z.uuidv4()});
+            const { id } = idShema.parse(request.params);
+
+            
+
+        } catch {
+            replay.status(401).send("Impossível deletar este livro. Tente novamente mais tarde!")
         }
     })
 }
