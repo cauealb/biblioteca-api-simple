e8758d0d3245aed889e39b3ccda665ed4e593153
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-06-01 22:28:37 -0300

    feat: Incluindo rota post e criando tipos

--- src/routes/libary.ts
+++ src/routes/libary.ts
@@ -1,10 +1,29 @@
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+
+interface Books {
+    id: number
+    title: string
+    author: string
+    publishedYear: Date
+    read: boolean
+}
+
+let books: Books[] = []
 
 export async function Libary(app: FastifyInstance) {
+    app.get('/', (_, replay) => {
+        replay.status(200).send(books);
+    })
 
-    app.get('/', (request, replay) => {
-        const body = request.body
+    app.post('/', (request, replay) => {
+        try {
+            const bodyShema = z.object({id: z.number, title: z.string().min(2), author: z.string(), publishedYear: z.date, read: z.boolean().default(false)})
+            const body = bodyShema.parse(request.body);
 
-        replay.status(200).send(body)
+            books.push(body);
+        } catch {
+            replay.status(401).send("Impossível incluir este livro. Tente novamente mais tarde!")
+        }
     })
 }
