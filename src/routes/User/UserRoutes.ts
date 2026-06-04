8418a31bbcc323809e286cab630c887e1a0afcee
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-06-03 22:27:18 -0300

    feat: Incluindo rota de visualização de 1 usuário

--- src/routes/User/UserRoutes.ts
+++ src/routes/User/UserRoutes.ts
@@ -2,50 +2,67 @@ import type { FastifyInstance } from "fastify";
 import { PrismaClient } from "@prisma/client";
 import z, { email } from "zod";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export async function UserRoutes(app: FastifyInstance) {
-    app.get('/', async (_, replay) => {
-        try {
-            const user = await prisma.user.findMany();
-            replay.status(404).send(user);
-        } catch {
-            replay.status(400).send("Erro ao visualizar usuários!")
-        }
-    })
+  app.get("/", async (_, replay) => {
+    try {
+      const user = await prisma.user.findMany();
+      replay.status(404).send(user);
+    } catch {
+      replay.status(400).send("Erro ao visualizar usuários!");
+    }
+  });
 
-    app.post('', async (request, replay) => {
-        try {
-            const requestSchema = z.object({
-                name: z.string(),
-                email: z.email(),
-                password: z.coerce.string()
-            })
+  app.get("/:id", async (request, replay) => {
+    try {
+      const idSchema = z.object({ id: z.number() });
+      const { id } = idSchema.parse(request.params);
 
-            const { name, email, password } = requestSchema.parse(request.body);
+      const user = await prisma.user.findFirst({
+        where: { idUser: id },
+      });
 
-            const user = await prisma.user.findFirst({
-                where: {
-                    email: email
-                }
-            })
+      if (user) {
+        replay.status(200).send(user);
+        return;
+      }
 
-            if (user) {
-                throw new Error();
-            }
+      replay.status(404).send();
+    } catch {
+      replay.status(400).send("Erro ao visualizar usuário!");
+    }
+  });
 
-            await prisma.user.create({
-                data: {
-                    name: name,
-                    email: email,
-                    password: password
-                }
-            })
+  app.post("", async (request, replay) => {
+    try {
+      const requestSchema = z.object({
+        name: z.string(),
+        email: z.email(),
+        password: z.coerce.string(),
+      });
 
-        } catch {
-            replay.status(400).send("Erro ao criar usuário!")
-        }
-    })
+      const { name, email, password } = requestSchema.parse(request.body);
 
-    
+      const user = await prisma.user.findFirst({
+        where: {
+          email: email,
+        },
+      });
+
+      if (user) {
+        throw new Error();
+      }
+
+      await prisma.user.create({
+        data: {
+          name: name,
+          email: email,
+          password: password,
+        },
+      });
+    } catch {
+      replay.status(400).send("Erro ao criar usuário!");
+    }
+  });
 }
